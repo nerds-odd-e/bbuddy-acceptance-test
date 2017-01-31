552dd14b68e8eb5b5214de2daf80ec580af4d0de
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-01-31 20:55:07 +0800

    Squashed 'api/' content from commit f587210
    git-subtree-dir: api
    git-subtree-split: f587210c8d7a26a8a31ab749bdeb3d04ba7029ea

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :accounts
+  mount_devise_token_auth_for 'User', at: 'auth'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
