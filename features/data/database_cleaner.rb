7e872fcd39e25b548c7d3e9a5fc9ee472370a731
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-03-12 20:57:32 +0800

    refactoring: now the database will be cleaned before each scenario too

--- features/data/database_cleaner.rb
+++ features/data/database_cleaner.rb
@@ -6,7 +6,3 @@ begin
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
-
-Around do |scenario, block|
-  DatabaseCleaner.cleaning(&block)
-end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,5 @@
 require_relative('../data/authentication')
 require_relative('../server/rails_server')
-require_relative('../data/database_cleaner')
 
 start_server
 
@@ -8,6 +7,15 @@ at_exit do
   stop_server
 end
 
+Before do |scenario|
+  DatabaseCleaner.clean!
+end
+
 Before ('@login') do
   LoginPage.open.login(default_user.save)
 end
+
+Around do |scenario, block|
+  DatabaseCleaner.cleaning(&block)
+end
+
