fda5b1e9c418a280a864d4e48486412d688ca627
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-04-03 20:39:19 +0800

    use test env for angular

--- features/z_angular/server/angular_server.rb
+++ features/z_angular/server/angular_server.rb
@@ -1,6 +1,6 @@
 def start_server
   `RAILS_ENV=test rails s -d -p 4000`
-  @gulp_pid = IO.popen("gulp --cwd ../bbuddy-angular &").pid
+  @gulp_pid = IO.popen("gulp --cwd ../bbuddy-angular --production &").pid
   sleep 10
 end
 
