7cdb91bdca91d43abd35f52a1837ec980be0c981
Jackson Zhang <zbcjackson@gmail.com>
2017-05-22 10:59:06 +0800

    Use spawn to start rails server and gulp server and Process.kill to kill processes

--- features/z_react/server/react_server.rb
+++ features/z_react/server/react_server.rb
@@ -1,13 +1,13 @@
 require 'optparse'
 
 def start_server
-  `RAILS_ENV=test rails s -d -p 4000`
+  @rails_pid = spawn({'RAILS_ENV' => 'test'}, 'rails s -p 4000')
   react_folder = ENV['REACT'] ? ENV['REACT'] : '../bbuddy-react'
-  @gulp_pid = IO.popen("gulp --cwd #{react_folder} --production &").pid
-  sleep 40
+  @gulp_pid = spawn("gulp server --cwd #{react_folder} --production")
+  sleep 10
 end
 
 def stop_server
-  `kill -9 $(cat tmp/pids/server.pid)`
-  `kill -9 #{@gulp_pid}`
+  Process.kill('KILL', @rails_pid)
+  Process.kill('KILL', @gulp_pid)
 end
