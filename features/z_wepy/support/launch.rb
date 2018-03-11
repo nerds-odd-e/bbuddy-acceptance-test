02fe79d05b19d2d13b7842f98e2a7dc00dd4e358
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-03-11 15:25:16 +0800

    wepy: implement the login

--- features/z_wepy/support/launch.rb
+++ features/z_wepy/support/launch.rb
@@ -4,5 +4,22 @@ caps[:caps]['goog:chromeOptions'.to_sym] = {androidProcess: 'com.tencent.mm:tool
 
 Appium::Driver.new(caps, true)
 
-Before { $driver.start_driver }
-After { $driver.driver_quit }
+Before {
+  $driver.start_driver
+  # $driver.find_element(:id, "com.tencent.mm:id/hf").click
+  $driver.find_element(:xpath, "//*[@text='发现']").click
+  $driver.find_element(:xpath, "//*[@text='小程序']").click
+  $driver.find_element(:xpath, "//*[@text='开发版']").click
+  email = $driver.find_element :xpath, "//*[@text='Input email here']"
+  email.click
+  email.send_keys('joseph.yao.ruozhou@gmail.com')
+  password = $driver.find_element :xpath, "//*[@text='Input password here']"
+  password.click
+  password.send_keys('123456')
+  $driver.find_element(:xpath, "//*[@text='Sign In']").click
+  sleep 5
+}
+
+After {
+  $driver.driver_quit
+}
